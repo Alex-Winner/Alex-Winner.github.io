53fa6b8b09d3ce3e1d99928473af3de164942248
Alex Volodarsky <78879697+Alex-Winner@users.noreply.github.com>
2023-10-23 17:21:21 +0300

    add contact form

--- js/script.js
+++ js/script.js
@@ -76,27 +76,28 @@ btns.forEach(btn => {
 
 
 
-/*==================== MODAL ====================*/
-var modal = document.getElementById("myModal");         // Get the modal
-var btn = document.getElementById("projectBtn");        // Get the button that opens the modal
-var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get all modal elements
+    const modals = document.querySelectorAll('.modal');
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+    modals.forEach(modal => {
+        const modalBtn = document.getElementById(modal.id + "-btn");
+        const span = modal.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+        modalBtn.onclick = function () {
+            modal.style.display = "block";
+        }
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+        window.addEventListener("click", function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        });
+    });
+});
 
 
 /*==================== QUALIFICATION TABS ====================*/
