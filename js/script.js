318e5e5ccf0b6007bdcf6f6007e2cc72e73ae617
Alex Volodarsky <78879697+Alex-Winner@users.noreply.github.com>
2023-10-30 11:22:04 +0200

    add small device support

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ window.addEventListener("scroll", function () {
 });
 
 
+function naVbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
